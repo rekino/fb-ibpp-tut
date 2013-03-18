8fe897a884ab1c2c85e0acdf6a29d83fae9513c7
Ramin Barati <rekino@gmail.com>
2013-03-18 12:07:10 +0430

    Creating frmItems

---
+++ c#/Autorun/DatabaseMaker/frmItem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatabaseMaker
+{
+    public partial class frmItem : Form
+    {
+        public frmItem()
+        {
+            InitializeComponent();
+        }
+
+        private void pLATFORMBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.pLATFORMBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dataSet);
+
+        }
+
+        private void iTEMBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.iTEMBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dataSet);
+
+        }
+
+        private void iTEMBindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.iTEMBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dataSet);
+
+        }
+
+        private void frmItem_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dataSet.ITEM' table. You can move, or remove it, as needed.
+            this.iTEMTableAdapter.Fill(this.dataSet.ITEM);
+
+        }
+    }
+}
