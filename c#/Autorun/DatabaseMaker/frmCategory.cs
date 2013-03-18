335ffccf63afc4d3133216470618103cca90b658
Ramin Barati <rekino@gmail.com>
2013-03-18 11:34:31 +0430

    Create frmCategory

---
+++ c#/Autorun/DatabaseMaker/frmCategory.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DatabaseMaker
+{
+    public partial class frmCategory : Form
+    {
+        public frmCategory()
+        {
+            InitializeComponent();
+        }
+
+        private void cATEGORYBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.cATEGORYBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dataSet);
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                    iMAGEPictureBox.Load(dialog.FileName);
+            }
+            catch
+            {
+                MessageBox.Show(
+                    "Image Format is not supported",
+                    "Error",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+    }
+}
